26019a0e32b0acc6315074f9b6fd6d465c1397a2
Egor.Bogomolov <egor.bogomolov@jetbrains.com>
2022-07-14 13:10:05 +0300

    Add testing mode to abstract model

--- models/transformers/abstract_ranking_model.py
+++ models/transformers/abstract_ranking_model.py
@@ -36,22 +36,36 @@ class AbstractRankingModel(pl.LightningModule, ABC):
     def validation_epoch_end(self, outputs):
         self._shared_epoch_end(outputs, "val")
 
+    def test_step(self, batch, batch_idx):
+        return self._shared_step(batch, batch_idx, "test")
+
+    def test_epoch_end(self, outputs):
+        self._shared_epoch_end(outputs, "test")
+
     def _shared_step(self, batch, batch_idx, stage):
         preds = self.forward(batch)
-        loss = self.loss_function(preds.view(-1), batch["score"].view(-1))
-        log = {f"{stage}/loss": loss}
 
-        if stage == "train":
-            self.log_dict(log, on_step=True, on_epoch=False)
+        if stage != "test":
+            loss = self.loss_function(preds.view(-1), batch["score"].view(-1))
+            log = {f"{stage}/loss": loss}
 
-        log["loss"] = loss
-        log["batched_predictions"] = (
-            preds.view(-1),
-            batch["score"].view(-1),
-            batch["md_count"],
-            batch["code_count"],
-            batch["notebook_id"],
-        )
+            if stage == "train":
+                self.log_dict(log, on_step=True, on_epoch=False)
+
+            log["loss"] = loss
+        else:
+            log = {}
+
+        log["batched_predictions"] = {
+            "preds": preds.view(-1),
+            "md_count": batch["md_count"],
+            "code_count": batch["code_count"],
+            "notebook_id": batch["notebook_id"],
+            "cell_id": batch["cell_id"],
+        }
+
+        if stage != "test":
+            log["batched_predictions"]["score"] = batch["score"].view(-1),
 
         return log
 
@@ -65,29 +79,47 @@ class AbstractRankingModel(pl.LightningModule, ABC):
 
         batched_preds = [output["batched_predictions"] for output in outputs]
 
-        preds = torch.cat([x[0] * (x[2] + x[3]) for x in batched_preds]).round()
-        scores = torch.cat([x[1] * (x[2] + x[3]) for x in batched_preds]).round()
-        md_counts = torch.cat([x[2] for x in batched_preds])
-        code_counts = torch.cat([x[3] for x in batched_preds])
+        if stage != "test":
+            scores = torch.cat([x["score"] * (x["md_count"] + x["code_count"]) for x in batched_preds]).round()
+
+        preds = torch.cat([x["preds"] * (x["md_count"] + x["code_count"]) for x in batched_preds]).round()
+        md_counts = torch.cat([x["md_count"] for x in batched_preds])
+        code_counts = torch.cat([x["code_count"] for x in batched_preds])
 
         total_inv, total_max_inv = 0, 0
 
-        notebook_ids = np.concatenate([x[-1] for x in batched_preds])
+        notebook_ids = np.concatenate([x["notebook_id"] for x in batched_preds])
+        cell_ids = np.concatenate([x["cell_id"] for x in batched_preds])
+
+        if stage == "test":
+            resulting_order = {}
+
         for notebook_id in np.unique(notebook_ids):
             loc = notebook_id == notebook_ids
             pred_positions = preds[loc]
-            true_positions = scores[loc]
             n_md = md_counts[loc][0].item()
             n_code = code_counts[loc][0].item()
 
-            true_order = OrderBuilder.greedy_ranked(true_positions, n_md, n_code)
             pred_order = OrderBuilder.greedy_ranked(pred_positions, n_md, n_code)
 
-            inv, max_inv = OrderBuilder.kendall_tau(true_order, pred_order)
-            total_inv += inv
-            total_max_inv += max_inv
+            if stage != "test":
+                true_positions = scores[loc]
+                true_order = OrderBuilder.greedy_ranked(true_positions, n_md, n_code)
+                inv, max_inv = OrderBuilder.kendall_tau(true_order, pred_order)
+                total_inv += inv
+                total_max_inv += max_inv
+            else:
+                cell_positions = cell_ids[loc]
+                for i, prediction in enumerate(pred_order):
+                    if prediction >= n_code:
+                        pred_order[i] = cell_positions[prediction - n_code]
 
-        kt = 1 - 4 * total_inv / total_max_inv
-        log[f"{stage}_kendall_tau"] = kt
+                resulting_order[notebook_id] = pred_order
 
-        self.log_dict(log, on_step=False, on_epoch=True)
+        if stage != "test":
+            kt = 1 - 4 * total_inv / total_max_inv
+            log[f"{stage}_kendall_tau"] = kt
+            self.log_dict(log, on_step=False, on_epoch=True)
+        else:
+            self.log_dict(resulting_order)
+
